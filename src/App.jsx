c3e263276f3f370790689227c8f99ab2366bb7a9
Arthur Willem <arthur.macaneiro@eaportal.org>
2024-03-29 01:34:49 -0300

    Terceiro commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ export default function App () {
   return (
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src={iconSedans} alt="ícone dos carros sedan" />
           <h2>Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na sua cidade ou na siua próxima viagem</p>
           <a href="https://www.webmotors.com.br/sedans">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-green">
           <img src={iconSuvs} alt="ícone dos carros SUV" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo o seu interior espaçoso, poder e versatilidade. PErfeito para passar as férias em família ou para aventuras off-road</p>
           <a href="https://www.webmotors.com.br/suvs">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxury} alt="ícone dos carros de luxo" />
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute de um conforto aprimorado de um aluguel de luxo e cheque com estilo</p>
